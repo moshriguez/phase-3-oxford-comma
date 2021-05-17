173b11b127a559e99191ed9aabfa7223a5a7f323
Marc Rodriguez <marc.herman.rodriguez@gmail.com>
2021-05-17 10:15:06 -0700

    completed

--- lib/oxford_comma.rb
+++ lib/oxford_comma.rb
@@ -1,3 +1,11 @@
 def oxford_comma(array)
-
+    if array.size < 2 
+        array.join
+    elsif array.size == 2
+        array.join(' and ')
+    else 
+        last_item = array.pop
+        array.push('and')
+        array.join(', ') + " #{last_item}"
+    end
 end
